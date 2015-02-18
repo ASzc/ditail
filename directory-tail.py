40023e024704ffd25d0bf9f6530a361909b380d6
Alex Szczuczko <alex@szc.ca>
2015-02-18 20:11:54 +0000

    Prototype for subprocess-free tail using Python replacement code.

--- directory-tail.py
+++ directory-tail.py
@@ -11,25 +11,42 @@ import pyinotify
 logger = logging.getLogger("dtail")
 
 @asyncio.coroutine
-def tail_task(path, loop, new=False):
-    cmd = ["tail", "-fq", "--pid", str(os.getpid()), "-n"]
-    if new:
-        cmd.append("+1")
-    else:
-        cmd.append("0")
-    cmd.append(path)
-    proc = yield from asyncio.create_subprocess_exec(*cmd, loop=loop, stdout=asyncio.subprocess.PIPE)
+def tail_task(path, modify_event, loop, new=False):
+    f = os.open(path, os.O_RDONLY | os.O_NONBLOCK)
     try:
-        line = yield from proc.stdout.readline()
-        while line:
-            sys.stdout.write(line)
-            line = yield from proc.stdout.readline()
-    finally:
-        proc.terminate()
+        if not new:
+            # Seek to end of file
+            os.lseek(f, 0, os.SEEK_END)
 
-def update_tasks_in(directory, tail_tasks, loop):
+        line_buffer = bytearray()
+        while True:
+            try:
+                block = os.read(f, 4096)
+            except BlockingIOError:
+                yield from modify_event.wait()
+                modify_event.clear()
+            else:
+                # Write any remaining bytes when EOF is encountered
+                if not block and line_buffer:
+                    sys.stdout.write(line_buffer)
+                    break
+
+                line_buffer.extend(block)
+
+                while True:
+                    n = line_buffer.find(b"\n")
+                    if n == -1:
+                        break
+                    else:
+                        # Write out through newline, remove that portion from buffer
+                        sys.stdout.write(line_buffer[:(n+1)])
+                        line_buffer = line_buffer[(n+1):]
+    finally:
+        os.close(f)
+
+def update_tasks_in(directory, tail_tasks, tail_events, loop):
     wm = pyinotify.WatchManager()
-    mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE
+    mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_MODIFY
 
     class Handler(pyinotify.ProcessEvent):
         def process_IN_CREATE(self, event):
@@ -40,7 +57,9 @@ def update_tasks_in(directory, tail_tasks, loop):
                     tail_tasks[path].cancel()
                     del tail_tasks[path]
                 logger.debug("Creating task for {}".format(path))
-                tail_tasks[path] = loop.create_task(tail_task(path, loop, new=True))
+                modify_event = asyncio.Event(loop=loop)
+                tail_events[path] = modify_event
+                tail_tasks[path] = loop.create_task(tail_task(path, modify_event, loop, new=True))
             else:
                 logger.debug("New path {} is not a file, ignoring".format(path))
 
@@ -53,6 +72,14 @@ def update_tasks_in(directory, tail_tasks, loop):
             else:
                 logger.debug("No task to cancel for {}".format(path))
 
+        def process_IN_MODIFY(self, event):
+            path = event.pathname
+            if path in tail_events:
+                logger.debug("Event set for {}".format(path))
+                tail_events[path].set()
+            else:
+                logger.debug("No event to set for {}".format(path))
+
     notifier = pyinotify.AsyncioNotifier(wm, loop, default_proc_fun=Handler())
     wm.add_watch(directory, mask, rec=True, auto_add=True)
 
@@ -64,13 +91,16 @@ def generate_files_in(directory):
 def tail_files_in(directory):
     loop = asyncio.get_event_loop()
     tail_tasks = dict()
+    tail_events = dict()
     abs_directory = os.path.abspath(directory)
 
     try:
         for path in generate_files_in(abs_directory):
             logger.debug("Creating initial task for {}".format(path))
-            tail_tasks[path] = loop.create_task(tail_task(path, loop))
-        update_tasks_in(abs_directory, tail_tasks, loop)
+            modify_event = asyncio.Event(loop=loop)
+            tail_events[path] = modify_event
+            tail_tasks[path] = loop.create_task(tail_task(path, modify_event, loop))
+        update_tasks_in(abs_directory, tail_tasks, tail_events, loop)
 
         logger.debug("Starting loop")
         loop.run_forever()
